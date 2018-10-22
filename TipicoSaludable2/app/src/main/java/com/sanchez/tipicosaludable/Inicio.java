e59efae0f9a2905ec35b71e79f67e41b82740796
Leansanchez <lsanchezorozco@misena.edu.co>
2018-10-22 16:59:57 -0500

    la cagada.exe se arreglo.io

--- TipicoSaludable2/app/src/main/java/com/sanchez/tipicosaludable/Inicio.java
+++ TipicoSaludable2/app/src/main/java/com/sanchez/tipicosaludable/Inicio.java
@@ -9,9 +9,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridView;
+import android.widget.TextView;
 
 
 public class Inicio extends Fragment {
+    int size;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -20,8 +22,24 @@ public class Inicio extends Fragment {
 
         View vista = inflater.inflate(R.layout.fragment_inicio, container, false);
         GridView gridView = (GridView) vista.findViewById(R.id.ultimoconsumo);
+
+        TextView textView = vista.findViewById(R.id.txtnoconsumo);
+
+
+        if (ActividadDetalle.ultimoconsumo.size()>0){
+            AdaptadorUltimoConsumo adaptador = new AdaptadorUltimoConsumo(getContext(),ActividadDetalle.ultimoconsumo);
+            gridView.setAdapter(adaptador);
+
+        }
+        else {
+            textView.setText("No has consumido nada hoy");
+
+        }
+
+
         AdaptadorUltimoConsumo adaptador = new AdaptadorUltimoConsumo(getContext(),ActividadDetalle.ultimoconsumo);
         gridView.setAdapter(adaptador);
+
 
 
         return vista;
