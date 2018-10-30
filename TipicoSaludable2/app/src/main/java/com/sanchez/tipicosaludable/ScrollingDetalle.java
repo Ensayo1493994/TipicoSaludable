f2a8d084f188e08b5bc088e358496f1f95381e87
Jhoan Alejandro Anaya <jaanaya88@misena.edu.co>
2018-10-30 09:36:45 -0500

    final_1.0

--- TipicoSaludable2/app/src/main/java/com/sanchez/tipicosaludable/ScrollingDetalle.java
+++ TipicoSaludable2/app/src/main/java/com/sanchez/tipicosaludable/ScrollingDetalle.java
@@ -177,8 +177,8 @@ public class ScrollingDetalle extends AppCompatActivity {
                     }
                     else {
 
-                        Calorias_consumidas = cantidaddelalimento*(consumo+ Integer.parseInt(informacion.getText().toString()));
-                        Toast.makeText(ScrollingDetalle.this, ""+Calorias_consumidas, Toast.LENGTH_SHORT).show();
+
+                        //Toast.makeText(ScrollingDetalle.this, ""+Calorias_consumidas, Toast.LENGTH_SHORT).show();
                         Calorias_consumidas = cantidaddelalimento*(consumo+ Double.parseDouble(informacion.getText().toString()));
                         //Toast.makeText(ActividadDetalle.this, ""+Calorias_consumidas, Toast.LENGTH_SHORT).show();
                         x=((CaloriasActivity.actmb*90)/100);
